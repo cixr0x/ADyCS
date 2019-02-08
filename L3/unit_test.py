ca72c8cdc16f3cd05d1dfa31b7b14f41b41a1458
Roberto Rojas <roberto.rojas@oracle.com>
2019-02-07 20:28:30 -0600

    Added L3

--- L3/unit_test.py
+++ L3/unit_test.py
@@ -3,6 +3,7 @@ import filecmp
 import unittest
 import sys
 import time
+import platform
 
 
 def ceil(number):
@@ -21,16 +22,22 @@ def file_compare(file1, file2):
     return filecmp.cmp(file1, file2)
 
 
+def time_clock():
+    return time.clock()
+
+
 class MathTest(unittest.TestCase):
     def test_ceil(self):
         self.assertEqual(ceil(100.000000000001), 101)
         self.assertEqual(ceil(999999.9), 1000000)
         self.assertEqual(ceil(1.000000000001), 2)
         self.assertEqual(ceil(float(sys.maxsize)+0.01), sys.maxsize+1)
+        self.assertEqual(ceil(-100.1), -100)
+        self.assertEqual(ceil(-0.1), 0)
+        self.assertEqual(ceil(0.0000000001), 1)
         # ???
         # self.assertEqual(ceil((float(sys.maxsize)*-1)),
         #                 (sys.maxsize*-1))
-
         with self.assertRaises(OverflowError):
             ceil(float("inf"))
         with self.assertRaises(TypeError):
@@ -54,12 +61,12 @@ class MathTest(unittest.TestCase):
         factorial(100000)
         duration = time.perf_counter() - start_time
         self.assertLess(duration, 1)
-        self.assertGreater(factorial(100000), 1000)
+        # just checking it doesnt crash
+        self.assertGreater(factorial(1000000), 1000)
 
     def test_pow(self):
         self.assertEqual(pow(2, 2), 4)
         self.assertEqual(pow(9, 9), 387420489)
-        # ??
         self.assertEqual(pow(0, 0), 1)
         self.assertEqual(pow(0, 1), 0)
         self.assertEqual(pow(1, 0), 1)
@@ -76,8 +83,55 @@ class MathTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             pow('a', 10)
 
-        def test_filecmp(self):
-            pass
+    def test_filecmp(self):
+        self.assertTrue(file_compare(
+            "filecmp_test1.txt", "filecmp_test1.txt"))
+        self.assertFalse(file_compare(
+            "filecmp_test1.txt", "filecmp_test2.txt"))
+        self.assertTrue(file_compare(
+            "filecmp_test2.txt", "filecmp_test2.txt"))
+        self.assertFalse(file_compare(
+            "filecmp_test2.txt", "filecmp_test5.pdf"))
+        self.assertTrue(file_compare(
+            "filecmp_test5.pdf", "filecmp_test5.pdf"))
+        self.assertFalse(file_compare(
+            "filecmp_test3.txt", "filecmp_test5.pdf"))
+        self.assertFalse(file_compare(
+            "filecmp_test3.txt", "filecmp_test4.app"))
+        self.assertFalse(file_compare(
+            False, "filecmp_test3.txt"))
+        # ??
+        # self.assertTrue(file_compare(
+        #    "filecmp_test4.app", "filecmp_test4.app"))
+        with self.assertRaises(TypeError):
+            file_compare(None, "filecmp_test3.txt")
+        with self.assertRaises(OSError):
+            file_compare(-10, "filecmp_test3.txt")
+
+    def test_time_clock(self):
+        time1 = time_clock()
+        time.sleep(0.1)
+        time2 = time_clock()
+        self.assertGreater(time2, time1)
+
+        # clock function works different in windows and unix based systems
+        if (platform.system() == "Windows"):
+            start = time_clock()
+            time.sleep(1)
+            duration = time_clock() - start
+            self.assertAlmostEqual(duration, 1)
+
+        time1 = time_clock()
+        time2 = time_clock()
+        self.assertAlmostEqual(time1, time2, 3)
+
+        time1 = time.time()
+        clock1 = time_clock()
+        factorial(50000)
+        diff_time = time.time() - time1
+        diff_clock = time_clock() - clock1
+        self.assertAlmostEqual(diff_clock, diff_time, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
